982ef680896d102a599a3538924ca0ac65926118
Eddie <eddie.ch.89d@gmail.com>
2021-01-18 14:32:53 +0100

    Course

--- course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/model/Course.java
+++ course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/model/Course.java
@@ -1,5 +1,137 @@
 package se.lexicon.course_manager_assignment.model;
 
+
+
+import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+
 public class Course {
+
+    private final int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+
+    Set<String> students = new HashSet<>();
+
+
+
+
+    public Course(int id) {
+        this.id = CourseSequencer.nextCourseId();
+    }
+
+
+
+    public Course(int id, String courseName, LocalDate startDate, int weekDuration, Set<String> students) {
+        this.id = CourseSequencer.nextCourseId();
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+        this.students = students;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+
+
+    public Set<String> getStudents() {
+        return students;
+    }
+
+
+
+    public void setStudents(Set<String> students) {
+        this.students = students;
+    }
+
+
+
+    public boolean enrollStudent(String student) {
+        return true;
+    }
+
+
+
+    public boolean unenrollStudent(String student) {
+        return true;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && weekDuration == course.weekDuration && Objects.equals(courseName, course.courseName) && Objects.equals(startDate, course.startDate) && Objects.equals(students, course.students);
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseName='" + courseName + '\'' +
+                ", startDate=" + startDate +
+                ", weekDuration=" + weekDuration +
+                ", students=" + students +
+                '}';
+    }
+
+
 
 }
