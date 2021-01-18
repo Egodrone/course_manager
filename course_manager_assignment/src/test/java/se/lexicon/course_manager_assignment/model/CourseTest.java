fa3e6eda9a1bc0b21c8446d7b9c9bdb646af7ce2
Eddie <eddie.ch.89d@gmail.com>
2021-01-18 15:13:11 +0100

    Junit test setup

--- course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/CourseTest.java
+++ course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/CourseTest.java
@@ -1,4 +1,22 @@
 package se.lexicon.course_manager_assignment.model;
 
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class CourseTest {
+
+    @Test
+    public void test_sequencer() {
+        int expected = 30;
+        int test = 0;
+        int counter = 0;
+
+        assertEquals(0, 0);
+    }
+
+
 }
--- course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -1,4 +1,21 @@
 package se.lexicon.course_manager_assignment.model;
 
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class StudentTest {
+
+
+    @Test
+    public void test_sequencer() {
+        int expected = 0;
+        assertEquals(0, 0);
+    }
+
+
+
 }
