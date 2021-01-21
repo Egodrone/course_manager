10ff0e6f7ff867c076505e180241d494d9b02abe
Eddie <eddie.ch.89d@gmail.com>
2021-01-21 19:07:38 +0100

    CourseCollectionRepository Test with Junit

--- course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepositoryTest.java
+++ course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/data/dao/CourseCollectionRepositoryTest.java
@@ -1,20 +1,46 @@
 package se.lexicon.course_manager_assignment.data.dao;
 
+
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.lexicon.course_manager_assignment.data.sequencers.CourseSequencer;
-
-
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
+import se.lexicon.course_manager_assignment.model.Course;
+import se.lexicon.course_manager_assignment.model.Student;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashSet;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+
+
 @SpringBootTest(classes = {CourseCollectionRepository.class})
+
+
+
 public class CourseCollectionRepositoryTest {
+
+
 
     @Autowired
     private CourseDao testObject;
+
+
+
+    @BeforeEach
+    void setUp() {
+        CourseSequencer.setCourseSequencer(0);
+        StudentSequencer.setStudentSequencer(0);
+    }
+
+
 
     @Test
     @DisplayName("Test context successfully setup")
@@ -22,7 +48,78 @@ public class CourseCollectionRepositoryTest {
         assertFalse(testObject == null);
     }
 
-    //Write your tests here
+
+
+    @Test
+    public void test_createCourse() {
+        Collection<Course> courses = new HashSet<>();
+        CourseCollectionRepository courseRepository = new CourseCollectionRepository(courses);
+        LocalDate date = LocalDate.parse("2021-01-21", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        Course c1 = courseRepository.createCourse("Java", date, 10);
+        String expected = "Course{id=1, courseName='Java', startDate=2021-01-21, weekDuration=10, students=[]}";
+        assertEquals(c1.toString(), expected);
+
+    }
+
+
+
+    @Test
+    public void test_findById() {
+        Collection<Course> courses = new HashSet<>();
+        CourseCollectionRepository courseRepository = new CourseCollectionRepository(courses);
+        LocalDate date = LocalDate.parse("2021-01-21", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        courseRepository.createCourse("Java", date, 15);
+        courseRepository.createCourse("Python", date, 10);
+        courseRepository.createCourse("BASH", date, 30);
+        Course c1 = courseRepository.findById(1);
+        String expected = "Course{id=1, courseName='Java', startDate=2021-01-21, weekDuration=15, students=[]}";
+        assertEquals(c1.toString(), expected);
+
+        // Test to find another course
+        c1 =  courseRepository.findById(3);
+        expected = "Course{id=3, courseName='BASH', startDate=2021-01-21, weekDuration=30, students=[]}";
+        assertEquals(c1.toString(), expected);
+    }
+
+
+
+    @Test
+    public void test_findByNameContains() {
+        Collection<Course> courses = new HashSet<>();
+        CourseCollectionRepository courseRepository = new CourseCollectionRepository(courses);
+        LocalDate date = LocalDate.parse("2021-01-21", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        courseRepository.createCourse("Java", date, 15);
+        courseRepository.createCourse("Python", date, 10);
+        courseRepository.createCourse("BASH", date, 30);
+
+        Collection<Course> expected = courseRepository.findByNameContains("Python");
+        assertEquals(expected.size(), 1);
+
+        // Test non existing course
+        expected = courseRepository.findByNameContains("C++");
+        assertEquals( 0, expected.size());
+    }
+
+
+
+    @Test
+    public void test_findByDateBefore() {
+        Collection<Course> courses = new HashSet<>();
+        CourseCollectionRepository courseRepository = new CourseCollectionRepository(courses);
+        LocalDate date = LocalDate.parse("2021-01-21", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate date2 = LocalDate.parse("2021-01-13", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate testDate = LocalDate.parse("2021-01-11", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        courseRepository.createCourse("Java", date, 15);
+        courseRepository.createCourse("Python", date2, 10);
+
+        Collection<Course> courseB = courseRepository.findByDateBefore(testDate);
+        String expectedStr = "[Course{id=1, courseName='Java', startDate=2021-01-21, weekDuration=15, students=[]}, " +
+                "Course{id=2, courseName='Python', startDate=2021-01-13, weekDuration=10, students=[]}]";
+        assertEquals(2, courseB.size());
+        assertEquals(expectedStr, courseB.toString());
+
+    }
+
 
 
     @AfterEach
@@ -30,4 +127,7 @@ public class CourseCollectionRepositoryTest {
         testObject.clear();
         CourseSequencer.setCourseSequencer(0);
     }
+
+
+
 }
