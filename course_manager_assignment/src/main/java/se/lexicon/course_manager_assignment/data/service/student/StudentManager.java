6117723958ccd1b2734e5d49976c5ac08404ecb1
Eddie <eddie.ch.89d@gmail.com>
2021-01-22 16:42:44 +0100

    StudentManager

--- course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/data/service/student/StudentManager.java
+++ course_manager_assignment/src/main/java/se/lexicon/course_manager_assignment/data/service/student/StudentManager.java
@@ -1,5 +1,7 @@
 package se.lexicon.course_manager_assignment.data.service.student;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.lexicon.course_manager_assignment.data.dao.CourseDao;
@@ -12,12 +14,18 @@ import se.lexicon.course_manager_assignment.dto.views.StudentView;
 
 import java.util.List;
 
+
+
+
 @Service
 public class StudentManager implements StudentService {
 
     private final StudentDao studentDao;
     private final CourseDao courseDao;
     private final Converters converters;
+
+
+
 
     @Autowired
     public StudentManager(StudentDao studentDao, CourseDao courseDao, Converters converters) {
@@ -26,6 +34,9 @@ public class StudentManager implements StudentService {
         this.converters = converters;
     }
 
+
+
+
     @Override
     public StudentView create(CreateStudentForm form) {
         //studentDao.createStudent(form.getName(), form.getEmail(), );
@@ -33,33 +44,53 @@ public class StudentManager implements StudentService {
         return null;
     }
 
+
+
     @Override
     public StudentView update(UpdateStudentForm form) {
         return null;
     }
+
+
+
 
     @Override
     public StudentView findById(int id) {
         return null;
     }
 
+
+
+
     @Override
     public StudentView searchByEmail(String email) {
         return null;
     }
+
+
+
 
     @Override
     public List<StudentView> searchByName(String name) {
         return null;
     }
 
+
+
+
     @Override
     public List<StudentView> findAll() {
         return null;
     }
 
+
+
+
     @Override
     public boolean deleteStudent(int id) {
         return false;
     }
+
+
+
 }
