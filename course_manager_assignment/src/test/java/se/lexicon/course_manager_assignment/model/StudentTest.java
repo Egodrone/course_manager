085edc701be1957b59545d19d4cb3cd8ed64a400
Eddie <eddie.ch.89d@gmail.com>
2021-01-25 21:28:16 +0100

    Student Test with Junit

--- course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ course_manager_assignment/src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -22,10 +22,7 @@ public class StudentTest {
 
     @Test
     public void test_setters_getters_id() {
-        Student s1 = new Student();
-        s1.setName("Rocky");
-        s1.setEmail("rockyBalboa@gmail.com");
-        s1.setAddress("Philadelphia");
+        Student s1 = new Student(1, "Rocky", "rockyBalboa@gmail.com", "Philadelphia");
         assertEquals("Rocky", s1.getName());
         assertEquals("rockyBalboa@gmail.com", s1.getEmail());
         assertEquals("Philadelphia", s1.getAddress());
@@ -36,8 +33,9 @@ public class StudentTest {
 
     @Test
     public void test_equals_hashCode() {
-        Student x = new Student("Rocky", "rockyBalboa@gmail.com", "Philadelphia");
-        Student y = new Student("Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+        Student x = new Student(1, "Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+        Student y = new Student(2, "Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+
         // Different id's should give false
         assertFalse(x.equals(y) && y.equals(x));
         assertFalse(x.hashCode() == y.hashCode());
@@ -47,9 +45,8 @@ public class StudentTest {
 
     @Test
     public void test_equals() {
-        System.out.println();
-        Student x = new Student("Rocky", "rockyBalboa@gmail.com", "Philadelphia");
-        Student y = new Student("Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+        Student x = new Student(1,"Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+        Student y = new Student(2,"Rocky", "rockyBalboa@gmail.com", "Philadelphia");
         assertTrue(x.getName().equals(y.getName()) && y.getName().equals(x.getName()));
         assertTrue(x.getEmail().equals(y.getEmail()) && y.getEmail().equals(x.getEmail()));
         assertTrue(x.getAddress().equals(y.getAddress()) && y.getAddress().equals(x.getAddress()));
@@ -59,9 +56,8 @@ public class StudentTest {
 
     @Test
     public void test_toString() {
-        Student x = new Student("Rocky", "rockyBalboa@gmail.com", "Philadelphia");
+        Student x = new Student(1,"Rocky", "rockyBalboa@gmail.com", "Philadelphia");
         String expected = "Student{id=1, name='Rocky', email='rockyBalboa@gmail.com', address='Philadelphia'}";
-        System.out.println(x.toString());
         assertEquals(expected, x.toString());
     }
 
